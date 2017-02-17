6d530865c228e30d8a748b03b88796ceef023d6b
Andrei Toledo <andreiltoledo@hotmail.com>
2017-02-17 17:24:23 -0200

    implementação da pesquisa de vinhos

--- src/main/java/com/andreitoledo/springmvc/vinhos/repository/Vinhos.java
+++ src/main/java/com/andreitoledo/springmvc/vinhos/repository/Vinhos.java
@@ -1,9 +1,13 @@
 package com.andreitoledo.springmvc.vinhos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.andreitoledo.springmvc.vinhos.model.Vinho;
 
 public interface Vinhos extends JpaRepository<Vinho, Long>{
+	
+	public List<Vinho> findByNomeContainingIgnoreCase(String nome);
 
 }
